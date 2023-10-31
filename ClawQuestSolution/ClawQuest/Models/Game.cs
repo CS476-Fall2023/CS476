0bb5784288874b61a6a4aff82a33ae4c25f3b3fe
TheBadFella <chashamnoor1@gmail.com>
2023-10-30 21:39:53 -0600

    Creating User, Game and Upgrade Models

---
+++ ClawQuestSolution/ClawQuest/Models/Game.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClawQuest.Models;
+
+public class Game
+{
+    public int GameId { get; set; }
+    public int UserId { get; set; }
+    public int Score { get; set; }
+    public int PlaysRemaining { get; set; }
+}
